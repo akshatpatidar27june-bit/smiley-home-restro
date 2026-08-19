ee331c3cd347c7117d7829d57499bd9a8f4f3520
akshatpatidar27june-bit <akshatpatidar27june@gmail.com>
2026-08-19 19:34:56 +0530

    Connect admin route and live decorations

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -2,6 +2,8 @@ import { InternetIdentityProvider } from "@caffeineai/core-infrastructure";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import AdminPanel from "./admin/AdminPanel";
+import DecorationGallery from "./admin/DecorationGallery";
 import "./index.css";
 
 BigInt.prototype.toJSON = function () {
@@ -15,11 +17,19 @@ declare global {
 }
 
 const queryClient = new QueryClient();
+const isAdminRoute = window.location.pathname.replace(/\/+$/, "") === "/admin";
+
+function PublicApp() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <InternetIdentityProvider>
+        <App />
+        <DecorationGallery />
+      </InternetIdentityProvider>
+    </QueryClientProvider>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <InternetIdentityProvider>
-      <App />
-    </InternetIdentityProvider>
-  </QueryClientProvider>,
+  isAdminRoute ? <AdminPanel /> : <PublicApp />,
 );
